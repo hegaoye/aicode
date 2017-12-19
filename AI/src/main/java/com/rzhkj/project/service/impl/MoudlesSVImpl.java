b95b3f395f35f6d148ecf8764d0ddfdeacb0be48
hegaoye <hegaoye@qq.com>
2017-12-19 14:50:13 +0800

    增加模块管理控制器

--- AI/src/main/java/com/rzhkj/project/service/impl/MoudlesSVImpl.java
+++ AI/src/main/java/com/rzhkj/project/service/impl/MoudlesSVImpl.java
@@ -9,8 +9,12 @@ package com.rzhkj.project.service.impl;
 import com.baidu.fsg.uid.UidGenerator;
 import com.rzhkj.core.base.BaseMybatisDAO;
 import com.rzhkj.core.base.BaseMybatisSVImpl;
+import com.rzhkj.core.exceptions.BaseException;
+import com.rzhkj.core.exceptions.MoudlesException;
+import com.rzhkj.core.tools.StringTools;
 import com.rzhkj.project.dao.MoudlesDAO;
 import com.rzhkj.project.entity.Moudles;
+import com.rzhkj.project.entity.MoudlesStateEnum;
 import com.rzhkj.project.service.MoudlesSV;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -33,4 +37,17 @@ public class MoudlesSVImpl extends BaseMybatisSVImpl<Moudles, Long> implements M
         return moudlesDAO;
     }
 
+    @Override
+    public void save(Moudles entity) throws BaseException {
+        if (entity == null
+                || StringTools.isEmpty(entity.getDescription())
+                || StringTools.isEmpty(entity.getName())) {
+            logger.error(BaseException.BaseExceptionEnum.Empty_Param.toString());
+            throw new MoudlesException(BaseException.BaseExceptionEnum.Empty_Param);
+        }
+
+        entity.setCode(String.valueOf(uidGenerator.getUID()));
+        entity.setState(MoudlesStateEnum.Enable.name());
+        super.save(entity);
+    }
 }

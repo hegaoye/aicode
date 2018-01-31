7e646d9a31fc33618bc392b129009ef2b6dd7f06
hegaoye <hegaoye@qq.com>
2018-01-31 11:22:34 +0800

    修改sql完整性

--- AI/src/main/java/com/rzhkj/project/service/impl/ProjectSqlSVImpl.java
+++ AI/src/main/java/com/rzhkj/project/service/impl/ProjectSqlSVImpl.java
@@ -6,12 +6,15 @@
 package com.rzhkj.project.service.impl;
 
 import com.baidu.fsg.uid.UidGenerator;
+import com.google.common.collect.Maps;
 import com.rzhkj.core.base.BaseMybatisDAO;
 import com.rzhkj.core.base.BaseMybatisSVImpl;
 import com.rzhkj.core.exceptions.BaseException;
 import com.rzhkj.core.exceptions.ProjectException;
 import com.rzhkj.core.tools.StringTools;
+import com.rzhkj.project.dao.ProjectDAO;
 import com.rzhkj.project.dao.ProjectSqlDAO;
+import com.rzhkj.project.entity.Project;
 import com.rzhkj.project.entity.ProjectSql;
 import com.rzhkj.project.entity.ProjectSqlStateEnum;
 import com.rzhkj.project.service.ProjectSqlSV;
@@ -19,6 +22,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 
 @Component
@@ -27,6 +31,9 @@ public class ProjectSqlSVImpl extends BaseMybatisSVImpl<ProjectSql, Long> implem
 
     @Resource
     private ProjectSqlDAO projectSqlDAO;
+
+    @Resource
+    private ProjectDAO projectDAO;
 
     @Resource
     private UidGenerator uidGenerator;
@@ -44,7 +51,39 @@ public class ProjectSqlSVImpl extends BaseMybatisSVImpl<ProjectSql, Long> implem
         }
         entity.setCode(String.valueOf(uidGenerator.getUID()));
         entity.setState(ProjectSqlStateEnum.Enable.name());
+        String tsql = entity.getTsql();
+        Map<String, Object> map = Maps.newHashMap();
+        map.put("projectCode", entity.getProjectCode());
+        Project project = projectDAO.load(map);
+        String sqlHeader = "";
+        //是否存在 drop  database  if  exists
+        tsql = tsql.replaceAll("\\s*(drop|DROP)\\s+(database|DATABASE)\\s+(if|IF){0,1}\\s+(exists|EXISTS){0,1}\\s+\\w+;{0,1}", "");
+        tsql = tsql.replaceAll("\\s*(create|CREATE)\\s+(database|DATABASE)\\s+\\w+;{0,1}", "");
+        tsql = tsql.replaceAll("\\s*(use|USE)\\s+\\w+;{0,1}", "");
+        sqlHeader = "DROP DATABASE if EXISTS " + project.getEnglishName() + ";\n";
+        sqlHeader += "CREATE DATABASE " + project.getEnglishName() + ";\n";
+        sqlHeader += "USE " + project.getEnglishName() + ";\n";
+        tsql = sqlHeader + " " + tsql;
+        entity.setTsql(tsql);
         super.save(entity);
+    }
+
+    public static void main(String[] args) {
+        String s = "    /*\n" +
+                "Navicat MariaDB Data Transfer\n" +
+                "\n" +
+                "Source Server         : 192.168.10.250\n" +
+                "Source Server Version : 100208\n" +
+                "Source Host           : 192.168.10.250:3306\n" +
+                "Source Database       : cycle\n" +
+                "\n" +
+                "Target Server Type    : MariaDB\n" +
+                "Target Server Version : 100208\n" +
+                "File Encoding         : 65001\n" +
+                "\n" +
+                "Date: 2018-01-23 09:02:02\n" +
+                "*/ sdfdf";
+        System.out.println(s.replaceAll("\\s*/\\*\\s*(.*\\n)*\\*/", ""));
     }
 
     /**

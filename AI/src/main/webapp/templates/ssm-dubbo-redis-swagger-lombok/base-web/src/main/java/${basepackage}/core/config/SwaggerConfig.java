d4323be33d98619ad94b48f591fe674374b4cd96
hegaoye <hegaoye@qq.com>
2018-02-01 09:52:11 +0800

    增加dubbo的模板init

--- AI/src/main/webapp/templates/ssm-dubbo-redis-swagger-lombok/base-web/src/main/java/${basepackage}/core/config/SwaggerConfig.java
+++ AI/src/main/webapp/templates/ssm-dubbo-redis-swagger-lombok/base-web/src/main/java/${basepackage}/core/config/SwaggerConfig.java
@@ -36,7 +36,7 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("庞帝中国 RESTful APIs")
-                .description("郑州仁中和科技有限公司保留所有代码著作权.如有任何疑问请访问官方网站与我们联系.代码只针对特定客户使用，不得在未经允许或授权的情况下对外传播扩散.恶意传播者，法律后果自行承担.本代码仅用于龐帝業務系统.请访问  http://tutors.ponddy.com/")
+                .description("郑州仁中和科技有限公司保留所有代码著作权.如有任何疑问请访问官方网站与我们联系.代码只针对特定客户使用，不得在未经允许或授权的情况下对外传播扩散.恶意传播者，法律后果自行承担.本代码仅用于AI-Code.请访问  http://tutors.ponddy.com/")
                 .contact(new Contact("郑州仁中和科技有限公司","http://www.rzhkj.com/","hegaoye@qq.com"))
                 .termsOfServiceUrl("http://tutors.ponddy.com/")
                 .version("1.0")

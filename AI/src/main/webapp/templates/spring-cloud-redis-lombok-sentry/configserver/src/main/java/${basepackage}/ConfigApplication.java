0744a3b5b62c5d0838b129fac1624f89a75c0f30
hegaoye <hegaoye@qq.com>
2018-06-29 11:50:43 +0800

    增加配置服务器

---
+++ AI/src/main/webapp/templates/spring-cloud-redis-lombok-sentry/configserver/src/main/java/${basepackage}/ConfigApplication.java
@@ -0,0 +1,36 @@
+package ${basePackage};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+/**
+ * 通过@EnableConfigServer注解激活配置服务.
+ * 说明：
+ * 在application.yml中有个git.uri的配置，目前配置的是https://github.com/eacdy/spring-cloud-study/
+ * 获取git上的资源信息遵循如下规则：
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ * <p>
+ * 例如本例：可使用以下路径来访问microservice-config-client-dev.properties：
+ * http://localhost:8765/application-dev.properties
+ * http://localhost:8765/consumer-dev.properties
+ * http://localhost:8765/consumer/dev
+ *
+ * @author eacdy
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}

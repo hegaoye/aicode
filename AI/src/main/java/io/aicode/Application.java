ec3ae13bdb02b9d822c71cb6ec4dee77bbb24295
shangze <nsh95958@163.com>
2019-07-06 14:25:38 +0800

    修改无法通过.shtml后缀访问问题

--- AI/src/main/java/io/aicode/Application.java
+++ AI/src/main/java/io/aicode/Application.java
@@ -2,11 +2,28 @@ package io.aicode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * 设置匹配*.shtml后缀请求
+     *
+     * @param dispatcherServlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean<DispatcherServlet> servletServletRegistrationBean = new ServletRegistrationBean<>(dispatcherServlet);
+        servletServletRegistrationBean.addUrlMappings("*.shtml");
+        return servletServletRegistrationBean;
     }
 }
---
+++ AI/src/main/java/io/aicode/base/MyWebMvcConfigurer.java
@@ -0,0 +1,15 @@
+package io.aicode.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        //开启路径后缀匹配
+        configurer.setUseRegisteredSuffixPatternMatch(true);
+    }
+}

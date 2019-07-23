ba18f9e6c210aa9cc8a449308b8961c891ae602e
hegaoye <hegaoye@qq.com>
2019-07-23 19:52:33 +0800

    优化sql

--- docker/ai_code.sql
+++ docker/ai_code.sql
@@ -1,5 +1,6 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+CREATE DATABASE IF NOT EXISTS ai_code DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for account

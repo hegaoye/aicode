788b971cdf4f5405d24682514654b95de1df62c4
jet <hegaoye@qq.com>
2020-10-22 18:43:34 +0800

    删除不必要的文件

--- aicode/src/main/java/com/aicode/Application.java
+++ aicode/src/main/java/com/aicode/Application.java
@@ -2,11 +2,7 @@ package com.aicode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
 public class Application {
 

76bbdb17a16b6b8df39029d2d7bbe3037b9cf5e4
abc <abc@gmail.com>
2023-09-05 17:45:01 +0800

    修改注解配置

--- aicode/src/main/java/com/aicode/Application.java
+++ aicode/src/main/java/com/aicode/Application.java
@@ -10,7 +10,10 @@ public class Application {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        while (true){
+            System.out.println(System.currentTimeMillis());
+        }
+//        SpringApplication.run(Application.class, args);
     }
 
 }
